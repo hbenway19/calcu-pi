984bed8eeb6d1f8eb62242603778d3c3d1744b5d
Guest User <coder@mainlab22.local>
2016-04-27 11:23:07 -0700

    dfgh

---
+++ GService Calculator/calculation.swift
@@ -0,0 +1,15 @@
+//
+//  calculation.swift
+//  GService Calculator
+//
+//  Created by Coder on 4/27/16.
+//  Copyright © 2016 Garrett Machen. All rights reserved.
+//
+
+
+import GameKit
+
+
+struct Numberss {
+    let numberrs = ["0", "1","2","3","4","5","6","7","8","9",""]
+}
